55bb466e7541f0e4db7d2029707c5b58ab8c504d
Vandana Nayak <nvandana1@gmail.com>
2024-01-12 20:07:44 +0530

    app title

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import TodoWrapper from './components/todoWrapper';
 
 function App() {
+  const title = 'Todo app'
   return (
     <div className="App">
       <header className="App-header">
-      <TodoWrapper></TodoWrapper>
+        <h2>{title}</h2>
       </header>
+      <TodoWrapper></TodoWrapper>
+
     </div>
   );
 }
